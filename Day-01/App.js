49b843ff7050117e3fd4fb099c77d5069d48121f
Kanika Sabharwal <sabhrwalkanika1999@gmail.com>
2025-04-27 19:27:37 +0530

    Day-01

---
+++ Day-01/App.js
@@ -0,0 +1,21 @@
+const parent = React.createElement("div", {id: "parent"}, 
+   [ [React.createElement("h1", {id: "child1"}, "Hello World"), 
+    React.createElement("h2", {id: "child2"}, "Hello World"),
+   ],[React.createElement("h1", {id: "child1"}, "Hello World"), 
+    React.createElement("h2", {id: "child2"}, "Hello World"),
+    ] ]);
+
+    // for nested elements use array
+
+
+
+
+
+
+
+
+
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World");
+        const roots = ReactDOM.createRoot(document.getElementById("root"));
+        roots.render(parent);
+        
